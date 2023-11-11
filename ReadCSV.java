7e149a7f58dad973c65421f47ccb6ce9b01dae81
theoreotically <jnoronha2601@gmail.com>
2023-11-11 17:47:00 -0600

    Added CSVReader

---
+++ ReadCSV.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+public class ReadCSV {
+  static double[][] csvArray = new double[1000][1500];
+
+  public double[][] readFile(String file) {
+
+    try {
+      File testFile = new File(file);
+      Scanner scanner = new Scanner(testFile);
+
+      scanner.useDelimiter(",");
+
+      int lineNum = 0;
+      while (scanner.hasNextLine()) {
+        String[] fields = scanner.nextLine().split(",");
+        double[] doubles = new double[fields.length];
+
+        for (int i = 0; i < fields.length; i++) {
+          doubles[i] = Double.parseDouble(fields[i]);
+        }
+
+        for (int i = 0; i < fields.length; i++) {
+          csvArray[lineNum] = doubles;
+        }
+        lineNum++;
+      }
+    } catch (FileNotFoundException e) {
+      System.out.print("Wrong");
+    }
+
+    return csvArray;
+  }
+}
