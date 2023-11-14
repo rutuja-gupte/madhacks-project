ed3d52e7f7b78eba5d5bb9456ded1c03ad68e205
Rutuja Gupte <rmgupte@wisc.edu>
2023-11-14 02:00:51 -0600

    Just trying some stuff out and fixed the plotting code

--- local_app.py
+++ local_app.py
@@ -13,6 +13,10 @@ import sqlite3
 import subprocess
 import io
 
+
+import json
+from collections import deque
+
 import numpy as np 
 from PIL import Image as im 
 
@@ -23,6 +27,62 @@ def find_num(string):
           return True
     return False
 
+def dist(start, target):
+    return ((start[0]-target[0])^2 + (start[1]-target[1])^2)^0.5
+
+def nextstep(arr2, curr):
+    mins = (0,0)
+    maxs = (arr2.shape)
+    
+    l = []
+    
+    # up
+    if curr[0] - 1 >= mins[0]:
+        if arr2[curr[0] - 1, curr[1]] == 255:
+            l.append((curr[0] - 1, curr[1]))
+    
+    # down
+    if curr[0] + 1 < maxs[0]:
+        if arr2[curr[0] + 1, curr[1]] == 255:
+            l.append((curr[0] + 1, curr[1]))
+    
+    # left
+    if curr[1] - 1 >= mins[1]:
+        if arr2[curr[0], curr[1] - 1] == 255:
+            l.append((curr[0], curr[1] - 1))
+    
+    # right
+    if curr[1] + 1 < maxs[1]:
+        if arr2[curr[0], curr[1] + 1] == 255:
+            l.append((curr[0], curr[1] + 1))
+            
+    return l
+
+def astar(arr, start, target):
+    todo = deque([(start, 0)])
+    visited = {}
+    visited[start] = None
+    
+    while len(todo) > 0:
+        item = todo.popleft()
+        curr = item[0]
+        if curr == target:
+            return backtrace(visited, start, target)
+        for child in nextstep(arr, curr):
+            if child not in visited:
+                visited[child] = curr
+                todo.append((child, item[1]+1))
+        todo = deque(sorted(todo, key = lambda i: i[1]))
+    
+    return "not found"
+
+def backtrace(d, start, dst):
+    curr = dst
+    path = (dst,)
+    while d[curr] != None:
+        path = (d[curr], ) + path
+        curr = d[curr]
+    return path
 
 @app.route('/')
 def home():
@@ -92,10 +152,10 @@ def mapping():
 
 #             path1 = subprocess.run(['java', '-cp', '.:AStar.jar:stdlib-package.jar', 'Asterist.AStarGraph', fname, src1, src2, dst1, dst2], check=True, stdout=subprocess.PIPE).stdout
 
-        src1 = str(int(df.loc[src]["top"])+50) 
-        src2 = str(int(df.loc[src]["left"])+50)
-        dst1 = str(int(df.loc[dst]["top"])-50)
-        dst2 = str(int(df.loc[dst]["left"])-50)
+        src1 = str(int(df.loc[src]["top"])) 
+        src2 = str(int(df.loc[src]["left"]))
+        dst1 = str(int(df.loc[dst]["top"]))
+        dst2 = str(int(df.loc[dst]["left"]))
         
 
 # #             change fname again
@@ -111,19 +171,29 @@ def mapping():
         
         path = str(path, encoding='utf-8')
         outs = path.split("\n")
-        paths = outs[0]
-        directions = outs[1]
-        
+#         paths = outs[0]
+#         directions = outs[1]
+
+######working
+        img = plt.imread(os.path.join('floorplans', table, fname))
+#         paths = astar(img[:,:], (int(src1), int(src2)), (int(dst1), int(dst2)))
+#         with open("temp.json", "w") as f:
+#             json.dump(paths, f)
+       #### end of working     
+            
+                  
         with open("temp.txt", "w") as f:
-            f.write(paths)
+            f.write(path)
     
 #         return return_string # gotta change this
+
+####working
         with open('result.html') as f:
             data = f.read()
         data = data.replace('??', fname)
         data = data.replace('###', table)
-#         data = data.replace('INSTRUCTIONS', path)
-        data = data.replace('INSTRUCTIONS', directions)
+        data = data.replace('INSTRUCTIONS', path)
+#         data = data.replace('INSTRUCTIONS', directions)
         return data
     with open("map.html") as f:
         html = f.read()
@@ -133,7 +203,7 @@ def mapping():
 
     return html
 
-@app.route('/dashboard.svg')
+@app.route('/dashboard.png')
 def dashboard():
     query_string = dict(flask.request.args)
     fname = query_string['fname']
@@ -147,25 +217,47 @@ def dashboard():
     l = re.findall('\(\d+,\d+\)', data)
     l2 = [item[1:-1].split(',') for item in l]
     l3 = [(int(item[0]), int(item[1])) for item in l2]
-    
-    
-  
-    img_test_result = img.copy()[:,:]
 
-    for x,y in l3:
-        img_test_result[x,y] = 127
+### Working code
+    with open("temp.json") as f:
+        l3 = json.load(f)    
+    print(l3)
+### Idk working code    
+    
+    
+#     img_test_result = img.copy()
+
+#     for x,y in l3:
+#         img_test_result[x,y] = 127
         
+### Working code
     figure = plt.figure()
     axes = figure.add_subplot(111)
-    for i in l3:
-        p = plt.Circle(i, 10, color='red')
-        axes.add_patch(p)
+### Idk working code    
+
+#     for i in l3:
+#         p = plt.Circle(i, 10, color='red')
+#         axes.add_patch(p)
 #     if len(l3) > 0:
 #         p1 = plt.Circle(l3[0], 10, color='red')
 #         p2 = plt.Circle(l3[-1], 10, color='red')
 #     axes.add_patch(p1)
 #     axes.add_patch(p2)
-    axes.imshow(img_test_result, cmap="viridis")
+
+    img5 = np.zeros((img.shape[0], img.shape[1], 3))
+    for x in range(img.shape[0]):
+        for y in range(img.shape[1]):
+            img5[x,y] = np.array([img[x,y], 255, 255])
+
+    for x,y in l3:
+        img5[x,y] = np.array([0, 0, 0])
+        
+#     img6 = im.fromarray(img5) 
+#     img6.save("temp.png")
+
+    axes.imshow(img5)
+
+#     axes.imshow(img_test_result, cmap="viridis")
 
 #     axes.imshow(img_test_result)
 #     return the plot by saving local copy
